52ac6438bc06243114e3143c4c3b4c60782c1379
Sam Apostel <sam@apostel.be>
2024-06-30 11:20:59 +0200

    hotfix duplicate step issue

--- packages/mdx/src/mdx-client/steps.tsx
+++ packages/mdx/src/mdx-client/steps.tsx
@@ -4,6 +4,10 @@ export function extractPreviewSteps(
   children: React.ReactNode,
   hasPreviewSteps?: boolean
 ) {
+  if (Array.isArray(children) && Array.isArray(children[0]) && children[0].length === 2) {
+    children = children.map(([actualStep]) => actualStep);
+  }
+  
   const allChildren = React.Children.toArray(children)
 
   const stepsChildren = hasPreviewSteps
